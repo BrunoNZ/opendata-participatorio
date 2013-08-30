5b262c058304051d02c2bc6082167cfc1f306bc6
Bruno Nocera Zanette <brunonzanette@gmail.com>
2013-08-30 11:41:32 -0300

    Add Calculation of Total execution time
    Added the calculation of total script execution time for logging purposes.
    
    Signed-off-by: Bruno Nocera Zanette <brunonzanette@gmail.com>

--- participatorio_opendata.py
+++ participatorio_opendata.py
@@ -22,26 +22,39 @@
 # USA.
 
 import MySQLdb
-from datetime import date
+import datetime
 
 from lib_user_section import write_users_section 
 from lib_group_section import write_groups_section 
 
 def main():
     
+    # Open connection to database
     db = MySQLdb.connect(host='localhost', \
                         user='opendata', \
                         passwd='123mudar', \
                         db='elgg', \
                         charset='utf8')
     
+    # Define directory and name of XML files
+    date_today=str(datetime.date.today())
     dir_results="/root/workdir_bnz07/xml_files/"
-    user_xml_file=dir_results+"usuarios_"+str(date.today())+".xml"
-    group_xml_file=dir_results+"comunidades_"+str(date.today())+".xml"
+    user_xml_file=dir_results+"usuarios_"+date_today+".xml"
+    group_xml_file=dir_results+"comunidades_"+date_today+".xml"
     
+    # Get the execution start time information
+    time_script_start=datetime.datetime.now()
+    
+    # Call functions to write XML files
     write_users_section(db,user_xml_file)
     write_groups_section(db,group_xml_file)
     
+    # Calculate and Print script time duration
+    script_duration=datetime.datetime.now()-time_script_start
+    total_exec_time=str(script_duration.total_seconds())
+    print "TOTAL EXECUTION TIME: "+total_exec_time+"\n"
+    
+    # Close database connection
     db.close()   
     
     return 0
